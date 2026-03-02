6e9a5cea4fb576d2f270ff10841785a1a37e7a96
SazaO_o <121901716+Saza-dev@users.noreply.github.com>
2026-03-02 14:54:20 +0530

    cookie configurations

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,18 +1,17 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
-export const generateToken = (userId,res) => {
-    const payload = {
-        id: userId
-    }
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN
-    })
+export const generateToken = (userId, res) => {
+  const payload = { id: userId };
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
-    res.cookie("jwt",token,{
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'strict',
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    })
-    return token
-}
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  });
+
+  return token;
+};
